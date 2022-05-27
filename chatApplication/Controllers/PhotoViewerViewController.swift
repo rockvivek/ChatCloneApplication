e366b39fcac88301c4ddd8426cb28802e70b1cec
rockvivek <vinodkumarshrivastawa@gmail.com>
2022-05-27 13:05:20 +0530

    structure created

---
+++ chatApplication/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViewerViewController.swift
+//  chatApplication
+//
+//  Created by vivek shrivastwa on 27/05/22.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ chatApplication/Controllers/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  chatApplication
+//
+//  Created by vivek shrivastwa on 27/05/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
